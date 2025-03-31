755eebc593f4c2ac0b3606858237ce3f974c1f97
Héctor <hgl502@hotmail.com>
2025-03-31 17:38:41 -0500

    importaciones absolutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: "https://jbloca.github.io/Landing-CentroMedico/",
-  base: "/Landing-CentroMedico/",
+  base: "Landing-CentroMedico",
   output: 'static',
   build: {
     outDir: './dist',
