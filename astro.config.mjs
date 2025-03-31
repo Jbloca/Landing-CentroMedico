b8df539405e53d9416d1970de340036b3f56a26e
Jorge Balarezo <127998906+Jbloca@users.noreply.github.com>
2025-03-31 16:09:44 -0500

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: "https://jbloca.github.io/Landing-CentroMedico/",
-    base: "/Landing-CentroMedico/", // Ruta base para GitHub Pages
-    output: 'static',
-    build: {
-      outDir: './dist',
-    },
-  });
-  
+  site: "https://jbloca.github.io/Landing-CentroMedico/",
+  base: "/Landing-CentroMedico/",
+  output: 'static',
+  build: {
+    outDir: './dist',
+  },
+});
